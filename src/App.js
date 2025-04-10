a4d8030506dfe3adb5948faeb3732c577c6e39e7
Ruslan Usman <your-email@example.com>
2025-04-10 23:34:22 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation,  } from 'react-router-dom';
+
+import React from 'react';
+
+
+import CalculatorExplosion from './components/CalculatorExplosion';
+import MainMenu from './components/MainMenu';
+import Results from './components/Results';
+
 import './App.css';
 
-function App() {
+const MainContent = () => {
+  const location = useLocation();
+
+  const hiddenFooterPaths = [
+    '/dragon-web-app', '/main-menu', '/materials', '/construction',
+    '/result', '/menu', '/calculator-explosion', '/results',
+    '/tal', '/instinct', '/intellect', '/fight', '/pricelist', '/talant-lios'
+  ];
+  const showFooter = !hiddenFooterPaths.includes(location.pathname);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <Routes>
+          <Route path="/" element={<MainMenu />} />
+          <Route path="/menu" element={<MainMenu />} />
+          <Route path="/calculator-explosion" element={<CalculatorExplosion />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </main>
+      {showFooter && (
+        <footer>
+          <p>© 2025 Dragon. Все права защищены.</p>
+          <div className="footer-buttons">
+            <Link to="/main-menu">
+              <button className="animated-button">Калькулятор рейда</button>
+            </Link>
+          </div>
+        </footer>
+      )}
+    </>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <header>
+          <nav>
+            <Link to="/">Главная</Link>
+          </nav>
+        </header>
+        <MainContent />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
